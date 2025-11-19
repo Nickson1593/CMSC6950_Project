fbbe7e57350efbb1a18e6d6424155065073d43c0
Tyler Nickson <tnickson@mun.ca>
2025-11-19 10:12:10 -0330

    Create 3D Surface Using Plotly

--- CMSC6950_Project.py
+++ CMSC6950_Project.py
@@ -23,5 +23,6 @@ X1, Y1 = np.meshgrid(x, y)
 #Apply Z-Values to XY Mesh
 Z1 = griddata(coordinates, Z, (X1, Y1), method='cubic')
 
-
+#Create 3D Surface Using Plotly
+fig = go.Figure(data=[go.Surface(x=X1, y=Y1, z=Z1,colorscale='hot')])
 
