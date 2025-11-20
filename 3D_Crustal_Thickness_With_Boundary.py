2faaa03077b6320ee8dbce4e5a0e1477cc5e818a
Tyler Nickson <tnickson@mun.ca>
2025-11-20 14:23:35 -0330

    Create a Grid Mask and Set Values Outside of the Boundary to NaN

--- 3D_Crustal_Thickness_With_Boundary.py
+++ 3D_Crustal_Thickness_With_Boundary.py
@@ -3,6 +3,7 @@ import numpy as np
 import plotly.graph_objects as go
 from scipy.interpolate import griddata
 from scipy.ndimage import gaussian_filter
+import pandas as pd
 
 #Load Crustal Thickness data from file
 data = np.loadtxt('Crustal_Thickness_CMSC6950.txt')
@@ -21,4 +22,14 @@ boundary_points = data_B[:, :2]
 
 #Create Study Area Boundary as a Path
 boundary = Path(boundary_points)
+
+#Identify Crustal Thickness XY Points Within the Boundary
+is_inside = boundary.contains_points(coordinates)
+
+#Make Copy of Crustal Thickness Data To Apply Mask
+masked_data = data.copy()
+
+#Apply Mask to Crustal Thickness Data (Make XY Points Outside of the Boundary = NaN)
+masked_data[~is_inside] = np.nan
+
 
