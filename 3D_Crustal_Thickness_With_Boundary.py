97875641d8fcac700c355a9ab3c174461d8169d8
Tyler Nickson <tnickson@mun.ca>
2025-11-20 14:16:08 -0330

    Load Data Files

---
+++ 3D_Crustal_Thickness_With_Boundary.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import plotly.graph_objects as go
+from scipy.interpolate import griddata
+from scipy.ndimage import gaussian_filter
+
+#Load Crustal Thickness data from file
+data = np.loadtxt('Crustal_Thickness_CMSC6950.txt')
+
+#Establish Crustal Thickness X, Y, Z Variables
+X = data[:, 0]
+Y = data[:, 1]
+Z = data[:, 2]
+
+#Load Study Area Boundary data from file
+data_B = np.loadtxt('basement_boundary_points.txt')
+
+#Estbalish Boundary X, Y Points
+boundary_points = data_B[:, :2]
+
+
