a692a8c9029a771b1d579b75b278ccbcd6039696
Tyler Nickson <tnickson@mun.ca>
2025-11-20 14:17:36 -0330

    Create Study Area Boundary as a Path

--- 3D_Crustal_Thickness_With_Boundary.py
+++ 3D_Crustal_Thickness_With_Boundary.py
@@ -11,6 +11,7 @@ data = np.loadtxt('Crustal_Thickness_CMSC6950.txt')
 X = data[:, 0]
 Y = data[:, 1]
 Z = data[:, 2]
+coordinates = data[:, :2]
 
 #Load Study Area Boundary data from file
 data_B = np.loadtxt('basement_boundary_points.txt')
@@ -18,4 +19,6 @@ data_B = np.loadtxt('basement_boundary_points.txt')
 #Estbalish Boundary X, Y Points
 boundary_points = data_B[:, :2]
 
+#Create Study Area Boundary as a Path
+boundary = Path(boundary_points)
 
