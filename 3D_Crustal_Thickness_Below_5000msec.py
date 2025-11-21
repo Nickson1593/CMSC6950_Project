091c531d29ded6c3824bf4b0bcdb960f5e1153c4
Tyler Nickson <tnickson@mun.ca>
2025-11-21 12:16:36 -0330

    Establish XY Coordinates for Crustal Thickness Data

--- 3D_Crustal_Thickness_Below_5000msec.py
+++ 3D_Crustal_Thickness_Below_5000msec.py
@@ -6,5 +6,8 @@ import plotly.graph_objects as go
 #Load Crustal Thickness data from file
 data = np.loadtxt('Crustal_Thickness_CMSC6950.txt')
 
+#Establish XY Coordinates
+coordinates = data[:, :2]
+
 
 
