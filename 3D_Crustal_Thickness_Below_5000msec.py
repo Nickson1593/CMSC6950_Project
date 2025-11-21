3226822f363c10e4e4c12b2b4a9954cad3700246
Tyler Nickson <tnickson@mun.ca>
2025-11-21 12:08:28 -0330

    Load Crustral Thickness Data

---
+++ 3D_Crustal_Thickness_Below_5000msec.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+
+#Load Crustal Thickness data from file
+data = np.loadtxt('Crustal_Thickness_CMSC6950.txt')
+
+
+
