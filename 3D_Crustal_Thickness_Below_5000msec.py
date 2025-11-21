fcca701f44624d24dd062f5999aaf98300230272
Tyler Nickson <tnickson@mun.ca>
2025-11-21 12:19:01 -0330

    Load Study Area Boundary Data, Create Mask and Apply to Crustal Thickness Data Outside the Study Area

--- 3D_Crustal_Thickness_Below_5000msec.py
+++ 3D_Crustal_Thickness_Below_5000msec.py
@@ -9,5 +9,29 @@ data = np.loadtxt('Crustal_Thickness_CMSC6950.txt')
 #Establish XY Coordinates
 coordinates = data[:, :2]
 
+#Load Study Area Boundary data from file
+data_B = np.loadtxt('basement_boundary_points.txt')
 
+#Estbalish Boundary X, Y Points
+boundary_points = data_B[:, :2]
+
+#Create Study Area Boundary as a Path
+boundary = Path(boundary_points)
+
+#Identify Crustal Thickness XY Points Within the Boundary
+is_inside = boundary.contains_points(coordinates)
+
+#Make Copy of Crustal Thickness Data To Apply Mask
+masked_data = data.copy()
+
+#Apply Mask to Crustal Thickness Data (Make XY Points Outside of the Boundary = NaN)
+masked_data[~is_inside] = np.nan
+
+#Use Pandas to Create a Data Frame and Delete Values = NaN
+df = pd.DataFrame(masked_data)
+df_cleaned = df.dropna()
+
+#Save & Load New XYZ File 
+df_cleaned.to_csv('cleaned_XYZ.csv', index=False)
+data_clean = np.loadtxt('cleaned_XYZ.txt')
 
