590b9efcd00d4f302c13c9bd11c2a2306d780183
Tyler Nickson <tnickson@mun.ca>
2025-11-25 14:39:10 -0330

    Load Data and Define Z Variable

--- Crustal_Thickness_Histogram.py
+++ Crustal_Thickness_Histogram.py
@@ -2,4 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Load data from file
+data = np.loadtxt('Crustal_Thickness_CMSC6950.txt')
+
+#Establish Z Variable
+Z = data[:, 2]
+
 
