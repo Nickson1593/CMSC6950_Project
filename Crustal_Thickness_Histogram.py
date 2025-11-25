00c3b6e8b13959eebb36cb964ba9c95dd645cf46
Tyler Nickson <tnickson@mun.ca>
2025-11-25 14:53:04 -0330

    Define Crustal Thickness Ranges

--- Crustal_Thickness_Histogram.py
+++ Crustal_Thickness_Histogram.py
@@ -3,9 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load data from file
-data = np.loadtxt('Crustal_Thickness_CMSC6950.txt')
+data = pd.read_csv('Crustal_Thickness_CMSC6950.txt')
 
 #Establish Z Variable
-Z = data[:, 2]
+Crustal_Thickness = data['Z'].values
 
-
+#Define Bin Ranges for Crustal Thickness
+Crustal_Thickness_Ranges = [0, 1.4, 2.8, 4.2, 5.6, 7.0, 8.4, 9.8, 11.2, 12.6, 14, 15.4, 16.8]
