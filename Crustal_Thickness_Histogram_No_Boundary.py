ba0dc4e44c00e6a459cc1accb49ddaac6c3aa1a5
Tyler Nickson <tnickson@mun.ca>
2025-11-28 15:13:17 -0330

    Define Bin Ranges

--- Crustal_Thickness_Histogram_No_Boundary.py
+++ Crustal_Thickness_Histogram_No_Boundary.py
@@ -13,4 +13,8 @@ mean_crustal_thickness = np.mean(Crustal_Thickness)
 median_crustal_thickness = np.median(Crustal_Thickness)
 std_dev_crustal_thickness= np.std(Crustal_Thickness)
 
+#Define Bin Ranges for Crustal Thickness
+bins = [0, 1400, 2800, 4200, 5600, 7000, 8400, 9800, 11200, 12600, 14000, 15400, 16800]
+bins_labels = ['0-1.4', '1.4-2.8', '2.8-4.2', '4.2-5.6', '5.6-7.0', '7.0-8.4', '8.4-9.8', '9.8-11.2', '11.2-12.6', '12.6-14.0', '14.0-15.4', '15.4-16.8']
+
 
