51a28eae5882315125f119d4e2eb8a0cee039f11
Tyler Nickson <tnickson@mun.ca>
2025-11-28 15:10:59 -0330

    Load Data File

--- Crustal_Thickness_Histogram_No_Boundary.py
+++ Crustal_Thickness_Histogram_No_Boundary.py
@@ -2,4 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#Load data from file
+data = pd.read_csv('Crustal_Thickness_CMSC6950.txt', sep='\s+', names=['X','Y','Z'])
+
 
