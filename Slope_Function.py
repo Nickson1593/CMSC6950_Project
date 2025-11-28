9b0a963bbb2730df0c9a6044b91f078d709e9812
Tyler Nickson <tnickson@mun.ca>
2025-11-28 14:06:39 -0330

    Update Libraries

--- Slope_Function.py
+++ Slope_Function.py
@@ -1,8 +1,6 @@
 import numpy as np
-import plotly.graph_objects as go
 from scipy.interpolate import griddata
 from scipy.ndimage import gaussian_filter
-from plotly.subplots import make_subplots
 
 #Define Slope Function
 def surface_slope(Crustal_Thickness_With_Boundary_XYZ):
