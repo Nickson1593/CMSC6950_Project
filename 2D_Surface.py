b83e4c780e0b4d83492f2898bd92c9f9a331e21d
Tyler Nickson <tnickson@mun.ca>
2025-11-28 15:38:30 -0330

    Update Plot Parameters

--- 2D_Surface.py
+++ 2D_Surface.py
@@ -10,12 +10,15 @@ Y = data[:, 1]
 Z = data[:, 2]
 
 #Create Scatter Plot
-plt.figure()
-scatter = plt.scatter(X, Y, c=Z, cmap='Spectral', s=10, edgecolors='none')
+plt.figure(figsize=(10,8))
+scatter = plt.scatter(X, Y, c=Z, cmap='Spectral', s=10, edgecolor='None'
  
 #Set Plot Paramaters
 plt.colorbar(scatter, label='Crustal Thickness (msec two-way travel time (TWT))')
+plt.ticklabel_format(style='plain', axis='x')
 plt.xlabel('Easting', fontsize=14)
 plt.ylabel('Northing', fontsize=14)
 plt.title('Crustal Thickness Map of the Orphan Basin', fontsize=22)
+plt.margins(x=0,y=0)
+plt.tight_layout()
 plt.show()
