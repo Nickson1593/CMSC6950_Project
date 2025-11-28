115d20e1e3dbbd96683235eac33f153bc22bd1d7
Tyler Nickson <tnickson@mun.ca>
2025-11-28 15:12:35 -0330

    Establish Z Variable and Compute Statistics

--- Crustal_Thickness_Histogram_No_Boundary.py
+++ Crustal_Thickness_Histogram_No_Boundary.py
@@ -5,4 +5,12 @@ import matplotlib.pyplot as plt
 #Load data from file
 data = pd.read_csv('Crustal_Thickness_CMSC6950.txt', sep='\s+', names=['X','Y','Z'])
 
+#Establish Z Variable
+Crustal_Thickness = data['Z'].values
+
+#Compute Meaningful Statistics
+mean_crustal_thickness = np.mean(Crustal_Thickness)
+median_crustal_thickness = np.median(Crustal_Thickness)
+std_dev_crustal_thickness= np.std(Crustal_Thickness)
+
 
