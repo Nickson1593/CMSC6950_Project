c0ed610ca9bcc0589702c04e0ff1eb79d746b1f6
Tyler Nickson <tnickson@mun.ca>
2025-11-28 14:04:56 -0330

    Define Gradients and Calculate Slopes

--- Slope_Function.py
+++ Slope_Function.py
@@ -9,14 +9,28 @@ def surface_slope(Crustal_Thickness_With_Boundary_XYZ):
     
     #Load Data File
     data = np.loadtxt('Crustal_Thickness_With_Boundary_XYZ.txt')
+    
     #Establish XYZ Variables
     X, Y, Z = data[:, 0], data[:, 1], data[:, 2]
+    
     #Create Grid Mesh
     x = np.linspace(X.min(), X.max(), 100)
     y = np.linspace(Y.min(), Y.max(), 100)
     X1, Y1 = np.meshgrid(x, y)
+    
     #Interpolate Z Values
     Z1 = griddata((X, Y), Z, (X1, Y1), method = 'cubic')
+    
     #Smooth Interpolation
     smoothed_Z1 = gaussian_filter(Z1, sigma = 0.8)
-    return X, Y, Z, slope
+    
+    #Define Gradients
+    dzdy, dzdx = np.gradient(smoothed_Z1, y[1] - y[0], x[1] - x[0])
+    
+    #Calculated Gradients
+    gradient = np.sqrt(dzdy**2 + dzdy**2)
+    
+    #Calculate Slopes
+    slope = np.degrees(np.arctan(gradient)
+    
+    return X1, Y1, smoothed_Z1, slope
