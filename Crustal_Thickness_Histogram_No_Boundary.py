c7aad08c113123c9f544cc73b319013ad42fe822
Tyler Nickson <tnickson@mun.ca>
2025-11-28 15:14:11 -0330

    Calculate Percentages of Points Within Each Range

--- Crustal_Thickness_Histogram_No_Boundary.py
+++ Crustal_Thickness_Histogram_No_Boundary.py
@@ -17,4 +17,9 @@ std_dev_crustal_thickness= np.std(Crustal_Thickness)
 bins = [0, 1400, 2800, 4200, 5600, 7000, 8400, 9800, 11200, 12600, 14000, 15400, 16800]
 bins_labels = ['0-1.4', '1.4-2.8', '2.8-4.2', '4.2-5.6', '5.6-7.0', '7.0-8.4', '8.4-9.8', '9.8-11.2', '11.2-12.6', '12.6-14.0', '14.0-15.4', '15.4-16.8']
 
+#Calculate Percentages of Points Within Each Range
+counts, range_edges= np.histogram(Crustal_Thickness, bins=bins)
+Total_Points = len(Crustal_Thickness)
+Percentages = (counts / Total_Points) * 100
+
 
