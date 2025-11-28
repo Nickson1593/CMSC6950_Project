64505550e91c3437d35139085fbeac5da04d7d4f
Tyler Nickson <tnickson@mun.ca>
2025-11-28 14:29:57 -0330

    Update 'Flat' 3D Surface Layout

--- Slope_Function_Plots.py
+++ Slope_Function_Plots.py
@@ -18,5 +18,13 @@ fig.update_layout(scene1=dict(xaxis=dict(title='Easting (m)', range=[420000, 995
                   yaxis=dict(title='Northing (m)', range=[5100000, 5800000]),zaxis=dict(title='Thickness (msec)', range=[0,12000])), width=1000, height=1000)
 fig.update_layout(scene1=dict(aspectmode='manual', aspectratio=dict(x=2,y=2,z=0.75)))
 
-#Create 'Flat' 3D Surface Layout (Z: Aspectratio = 0.001)
+#Create 'Flat' 3D Surface (Z: Aspectratio = 0.001)
 fig.add_trace(go.Surface(x=X, y=Y, z=Z, surfacecolor=Slope, colorscale='Picnic', colorbar=dict(title='Slope')), row=2, col=1)
+
+#Update 'Flat' 3D Surface Layout
+fig.update_layout(scene2=dict(xaxis=dict(title='Easting (m)', range=[420000, 995000]),
+                  yaxis=dict(title='Northing (m)', range=[5100000, 5800000]),zaxis=dict(title='Thickness (msec)', range=[0,12000])), width=1000, height=1000)
+fig.update_layout(scene2=dict(aspectmode='manual', aspectratio=dict(x=2,y=2,z=0.001)))
+
+#Show Figure
+fig.show()
