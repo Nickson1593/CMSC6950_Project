5a97b4415d198587414184a796dab6b52787e737
Tyler Nickson <tnickson@mun.ca>
2025-11-29 16:58:39 -0330

    Calculate Stretching Factors

--- Crustal_Thickness_Line_Graphs.py
+++ Crustal_Thickness_Line_Graphs.py
@@ -20,4 +20,9 @@ Z3 = df['Z3']
 #Define Initial Thickness of Undeformed Crust
 initial_thickness = 10557.39883
 
+#Calculate Stretching Factors
+stretching_factor_Z1 = initial_thickness / df['Z1']
+stretching_factor_Z2 = initial_thickness / df['Z2']
+stretching_factor_Z3 = initial_thickness / df['Z3']
+
 
