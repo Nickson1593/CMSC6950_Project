0c431291344765c92e96289cd255434da3e0a2f9
Tyler Nickson <tnickson@mun.ca>
2025-11-29 16:47:05 -0330

    Update

--- 3D_Crustal_Thickness_Below_2800msec.py
+++ 3D_Crustal_Thickness_Below_2800msec.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.graph_objects as go
+from matplotlib.path import Path
 
 #Load Crustal Thickness data from file
 data = np.loadtxt('Crustal_Thickness_CMSC6950.txt')
