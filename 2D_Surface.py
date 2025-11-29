1e2077639bd01990c5f69537463a4b61da866e82
Tyler Nickson <tnickson@mun.ca>
2025-11-29 11:39:38 -0330

    Create Surface Contours

--- 2D_Surface.py
+++ 2D_Surface.py
@@ -21,7 +21,11 @@ Z1 = griddata((X, Y), Z, (X1,Y1), method='linear')
 #Create Scatter Plot
 plt.figure(figsize=(10,8))
 scatter = plt.scatter(X, Y, c=Z, cmap='Spectral', s=10, edgecolor='None')
- 
+
+#Create Surface Contours
+contour = plt.contour(X1, Y1, Z1, levels=10, colors='black', linestyles='solid', linewidths=0.8)
+plt.clabel(contour, inline=True, fontsize=10, fmt='%1.0f')
+
 #Set Plot Paramaters
 plt.colorbar(scatter, label='Crustal Thickness (msec two-way travel time (TWT))')
 plt.ticklabel_format(style='plain', axis='x')
