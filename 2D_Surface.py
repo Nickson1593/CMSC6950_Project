41387c5442ba418659787439122d3d129b78ca84
Tyler Nickson <tnickson@mun.ca>
2025-11-29 11:49:58 -0330

    Update

--- 2D_Surface.py
+++ 2D_Surface.py
@@ -16,31 +16,31 @@ y = np.linspace(Y.min(), Y.max(), 100)
 X1, Y1 = np.meshgrid(x, y)
 
 #Interpolate Z Values
-Z1 = griddata((X, Y), Z, (X1,Y1), method='linear')
+Z1 = griddata((X, Y), Z, (X1,Y1), method = 'linear')
 
 #Create Scatter Plot
 plt.figure(figsize=(10,8))
-scatter = plt.scatter(X, Y, c=Z, cmap='Spectral', s=10, edgecolor='None')
+scatter = plt.scatter(X, Y, c = Z, cmap = 'Spectral', s = 10, edgecolor = 'None')
 
 #Colorbar Parameters
-cbar = plt.colorbar(scatter, label='Crustal Thickness (msec two-way travel time (TWT))')
+cbar = plt.colorbar(scatter, label = 'Crustal Thickness (msec two-way travel time (TWT))')
 cbar.set_label('Crustal Thickness (msec two-way travel time (TWT))', fontsize = 16)
-cbar.ax.tick_params(labelsize=12)
+cbar.ax.tick_params(labelsize = 12)
 
 #Create Surface Contours
-contour = plt.contour(X1, Y1, Z1, levels=10, colors='black', linestyles='solid', linewidths=0.8)
-plt.clabel(contour, inline=True, fontsize=10, fmt='%1.0f')
+contour = plt.contour(X1, Y1, Z1, levels = 10, colors = 'black', linestyles = 'solid', linewidths = 0.8)
+plt.clabel(contour, inline = True, fontsize = 10, fmt = '%1.0f')
 
 #Set Plot Paramaters
 plt.tick_params(axis = 'x', labelsize = 12)
 plt.tick_params(axis = 'y', labelsize = 12)
 plt.ticklabel_format(style = 'plain', axis = 'x')
 plt.ticklabel_format(style = 'plain', axis = 'y')
-plt.xlabel('Easting (m)', fontsize=16)
-plt.ylabel('Northing (m)', fontsize=16)
-plt.title('Crustal Thickness Map of the Orphan Basin', fontsize=23)
+plt.xlabel('Easting (m)', fontsize = 16)
+plt.ylabel('Northing (m)', fontsize = 16)
+plt.title('Crustal Thickness Map of the Orphan Basin', fontsize = 23)
 
 #Plot Format
-plt.margins(x=0,y=0)
+plt.margins(x = 0, y = 0)
 plt.tight_layout()
 plt.show()
