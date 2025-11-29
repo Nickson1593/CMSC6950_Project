4f5951d6b39d8d603ba89fe274278e82ca518241
Tyler Nickson <tnickson@mun.ca>
2025-11-29 14:45:07 -0330

    Update

--- 3D_Crustal_Thickness_With_Boundary.py
+++ 3D_Crustal_Thickness_With_Boundary.py
@@ -53,10 +53,10 @@ y = np.linspace(Y_clean.min(), Y_clean.max(), 100)
 X1, Y1 = np.meshgrid(x, y)
 
 #Use scipy.interpolate (griddata) to Interpolate Masked XYZ Data
-Z1_clean = griddata(coordinates_clean, Z_clean, (X1, Y1), method='linear')
+Z1_clean = griddata(coordinates_clean, Z_clean, (X1, Y1), method = 'linear')
 
 #Smooth Interpolation
-smoothed_Z1_clean = gaussian_filter(Z1_clean, sigma=0.2)
+smoothed_Z1_clean = gaussian_filter(Z1_clean, sigma = 0.2)
 
 #Define Tick Values
 xtick_values = [400000, 500000, 600000, 700000, 800000, 900000, 1000000]
@@ -69,20 +69,16 @@ ztick_values = [10000, 8000, 6000, 4000, 2000, 0]
 ztick_text = ['10000', '8000', '6000', '4000', '2000', '0']
 
 #Create 3D Surface Using Plotly
-fig = go.Figure(data=[go.Surface(x=X1, y=Y1, z=smoothed_Z1_clean,colorscale='Spectral', colorbar=dict(tickvals=ztick_values, ticktext=ztick_text,title_side='right', title=dict(text='Crustal Thickness (msec)', font=dict(size=20)),tickfont=dict(size=16)), contours = {"z":{"show": True, "start": 0, "end": 11000, "size": 1500}})])
+fig = go.Figure(data = [go.Surface(x = X1, y = Y1, z = smoothed_Z1_clean, colorscale = 'Spectral', colorbar = dict(tickvals = ztick_values, ticktext = ztick_text, title_side = 'right', title = dict(text = 'Crustal Thickness (msec)', font = dict(size = 20)),tickfont = dict(size = 16)), contours = {"z" : {"show" : True, "start" : 0, "end" : 11000, "size" : 1500}})])
 
 #Update the 3D Surface Layout 
-fig.update_layout(scene=dict(xaxis=dict(tickmode='array', tickvals=xtick_values, ticktext=xtick_text, title=dict(text='Easting (m)', font=dict(size=18)), range=[420000, 995000], tickfont=dict(size=13)),
-                  yaxis=dict(tickmode='array', tickvals=ytick_values, ticktext=ytick_text, title=dict(text='Northing (m)', font=dict(size=18)), range=[5180000, 5750000], tickfont=dict(size=13)),zaxis=dict(tickmode='array', tickvals=ztick_values, ticktext=ztick_text, title=dict(text='Thickness (msec)', font=dict(size=18)), range=[0,11000], tickfont=dict(size=13))), width=1000, height=800)
+fig.update_layout(scene = dict(xaxis = dict(tickmode = 'array', tickvals = xtick_values, ticktext = xtick_text, title = dict(text = 'Easting (m)', font = dict(size = 18)), range = [420000, 995000], tickfont = dict(size = 13)), yaxis = dict(tickmode = 'array', tickvals = ytick_values, ticktext = ytick_text, title = dict(text = 'Northing (m)', font = dict(size = 18)), range = [5180000, 5750000], tickfont = dict(size = 13)), zaxis = dict(tickmode = 'array', tickvals = ztick_values, ticktext = ztick_text, title = dict(text = 'Thickness (msec)', font = dict(size = 18)), range = [0, 11000], tickfont = dict(size = 13))), width = 1000, height = 800)
                   
 #Update the Aspect Ratio
-fig.update_layout(scene=dict(aspectmode='manual', aspectratio=dict(x=3,y=3,z=1)))
+fig.update_layout(scene = dict(aspectmode = 'manual', aspectratio = dict(x = 3, y = 3, z = 1)))
 
 #Set Camera Projection
-fig.update_layout(autosize=True, scene_camera_eye=dict(x=-1.8, y=-3, z=5))
+fig.update_layout(autosize = True, scene_camera_eye = dict(x = -1.8, y = -3, z = 5))
 
 #Show Figure
 fig.show()
-
-
-
