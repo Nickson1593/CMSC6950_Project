9f7444426518048ff2334e9f0d1de42c563bc892
Tyler Nickson <tnickson@mun.ca>
2025-11-29 17:29:43 -0330

    Update Plot Parameters and Add Legends

--- Crustal_Thickness_Line_Graphs.py
+++ Crustal_Thickness_Line_Graphs.py
@@ -30,3 +30,55 @@ max_stretching_factor = 7.0
 clipped_stretching_factor_Z1 = np.clip(stretching_factor_Z1, a_min=None, a_max=max_stretching_factor)
 clipped_stretching_factor_Z2 = np.clip(stretching_factor_Z2, a_min=None, a_max=max_stretching_factor)
 clipped_stretching_factor_Z3 = np.clip(stretching_factor_Z3, a_min=None, a_max=max_stretching_factor)
+
+#Setup Line Graphs
+fig, axs = plt.subplots(3, 1, figsize=(16,14), sharex=True)
+
+#Plot Line 1
+ax1 = axs[0]
+ax1.tick_params(axis='x', labelsize=14)
+ax1.set_xlabel('Easting (m)', fontsize=18)
+line1, = ax1.plot(df['X1'], df['Z1'], color='red', label='Crustal Thickness')
+ax1_twin = ax1.twinx()
+ax1_twin.set_ylabel('Stretching Factor', fontsize=18)
+ax1_twin.tick_params(axis='y', labelsize=14)
+line2, = ax1_twin.plot(df['X1'], clipped_stretching_factor_Z1, color='red', linestyle='--', label='Stretching Factor')
+
+#Lines 1 & 2 Legend
+lines = [line1, line2]
+labels = [l.get_label() for l in lines] 
+ax1.legend(lines, labels, loc='best') 
+
+#Plot Line 2
+ax2 = axs[1]
+ax2.tick_params(axis='x', labelsize=14)
+ax2.set_xlabel('Easting (m)', fontsize=18)
+line3, = ax2.plot(df['X2'], df['Z2'], color='orange', label='Crustal Thickness')
+ax2_twin = ax2.twinx()
+ax2_twin.set_ylabel('Stretching Factor', fontsize=18)
+ax2_twin.tick_params(axis='y', labelsize=14)
+line4, = ax2_twin.plot(df['X2'], clipped_stretching_factor_Z2, color='orange', linestyle='--', label='Stretching Factor')
+
+#Lines 3 & 4 Legend
+lines = [line3, line4]
+labels = [l.get_label() for l in lines] 
+ax2.legend(lines, labels, loc='best') 
+
+#Plot Lines 5 & 6
+ax3 = axs[2]
+ax3.tick_params(axis='x', labelsize=14)
+ax3.set_xlabel('Easting (m)', fontsize=18)
+line5, = ax3.plot(df['X3'], df['Z3'], color='blue', label='Crustal Thickness')
+ax3_twin = ax3.twinx()
+ax3_twin.set_ylabel('Stretching Factor', fontsize=18)
+ax3_twin.tick_params(axis='y', labelsize=14)
+line6, = ax3_twin.plot(df['X3'], clipped_stretching_factor_Z3, color='blue', linestyle='--', label='Stretching Factor')
+
+#Lines 5 & 6 Legend
+lines = [line5, line6]
+labels = [l.get_label() for l in lines] 
+ax3.legend(lines, labels, loc='best') 
+
+#Format Layout
+plt.tight_layout()
+plt.show()
