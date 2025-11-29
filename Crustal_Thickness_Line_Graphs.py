a573342223a5cee2a211fb3793eeec4ebc2f8db6
Tyler Nickson <tnickson@mun.ca>
2025-11-29 16:55:17 -0330

    Read Crustal Thickness Data File

---
+++ Crustal_Thickness_Line_Graphs.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+
+#Read Crustal Thickness data from file
+df = pd.read_csv('Crustal_Thickness_Supplementary_Data.csv', sep='\s+', header=None)
+
+
