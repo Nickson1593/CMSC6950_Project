46ff45cb671a99dc3e9ccff342f2cab91d10f485
Tyler Nickson <tnickson@mun.ca>
2025-11-29 16:07:55 -0330

    Update Surface Layout and Parameters

--- Slope_Function_Plots.py
+++ Slope_Function_Plots.py
@@ -2,29 +2,30 @@
 %load Slope_Function.py
 
 import plotly.graph_objects as go
-from plotly.subplots import make_subplots
+
+xtick_values = [400000, 500000, 600000, 700000, 800000, 900000, 1000000]
+xtick_text = ['400000', '500000', '600000', '700000', '800000', '900000', '1000000']
+
+ytick_values = [5100000, 5200000, 5300000, 5400000, 5500000, 5600000, 5700000, 5800000]
+ytick_text = ['5100000', '5200000', '5300000', '5400000', '5500000', '5600000', '5700000', '5800000']
+
+ztick_values = [10000, 8000, 6000, 4000, 2000, 0]
+ztick_text = ['10000', '8000', '6000', '4000', '2000', '0']
 
 #Define Variables Using Slope Function
 X,Y,Z,Slope = surface_slope('Crustal_Thickness_With_Boundary_XYZ.txt')
 
-#Make Subplots
-fig = make_subplots(rows=2, cols=1,specs=[[{'type':'surface'}],[{'type':'surface'}]], subplot_titles=[('Aspect Ratio 2:2:0.75'),('Aspect Ratio 2:2:0.001')], horizontal_spacing=0.01, vertical_spacing=0.05)
-
 #Create 3D Surface
-fig.add_trace(go.Surface(x=X, y=Y, z=Z, surfacecolor=Slope, colorscale='Picnic', colorbar=dict(title='Slope')), row=1, col=1)
+fig = go.Figure(data = [go.Surface(x = X, y = Y, z = Z, surfacecolor = Slope, colorscale = 'Picnic', colorbar = dict( title_side = 'right', title = dict(text = 'Slope', font = dict(size = 20)),tickfont = dict(size = 16)))])
 
 #Update 3D Surface Layout
-fig.update_layout(scene1=dict(xaxis=dict(title='Easting (m)', range=[420000, 995000]),
-                  yaxis=dict(title='Northing (m)', range=[5100000, 5800000]),zaxis=dict(title='Thickness (msec)', range=[0,12000])), width=1000, height=1000)
-fig.update_layout(scene1=dict(aspectmode='manual', aspectratio=dict(x=2,y=2,z=0.75)))
+fig.update_layout(scene = dict(xaxis = dict(tickmode = 'array', tickvals = xtick_values, ticktext = xtick_text, title = dict(text = 'Easting (m)', font = dict(size = 18)), range = [420000, 995000], tickfont = dict(size = 13)), yaxis = dict(tickmode = 'array', tickvals = ytick_values, ticktext = ytick_text, title = dict(text = 'Northing (m)', font = dict(size = 18)), range = [5180000, 5750000], tickfont = dict(size = 13)), zaxis = dict(tickmode = 'array', tickvals = ztick_values, ticktext = ztick_text, title = dict(text = 'Thickness (msec)', font = dict(size = 18)), range = [0, 11000], tickfont = dict(size = 13))), width = 1000, height = 800)
 
-#Create 'Flat' 3D Surface (Z: Aspectratio = 0.001)
-fig.add_trace(go.Surface(x=X, y=Y, z=Z, surfacecolor=Slope, colorscale='Picnic', colorbar=dict(title='Slope')), row=2, col=1)
+#Set Aspect Ratio
+fig.update_layout(scene = dict(aspectmode = 'manual', aspectratio = dict(x = 3, y = 3, z = 1)))
 
-#Update 'Flat' 3D Surface Layout
-fig.update_layout(scene2=dict(xaxis=dict(title='Easting (m)', range=[420000, 995000]),
-                  yaxis=dict(title='Northing (m)', range=[5100000, 5800000]),zaxis=dict(title='Thickness (msec)', range=[0,12000])), width=1000, height=1000)
-fig.update_layout(scene2=dict(aspectmode='manual', aspectratio=dict(x=2,y=2,z=0.001)))
+#Set Camera Projection
+fig.update_layout(autosize = True, scene_camera_eye = dict(x = -2.6, y = -3, z = 3))
 
 #Show Figure
 fig.show()
